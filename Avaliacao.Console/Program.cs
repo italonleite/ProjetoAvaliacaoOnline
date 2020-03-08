23f2ac295753dfa98c17c18afc517704bc4f8ea8
Italo Nunes Leite <italonleite@gmail.com>
2020-03-08 11:30:26 -0300

    Add project files.

---
+++ Avaliacao.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Avaliacao.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
