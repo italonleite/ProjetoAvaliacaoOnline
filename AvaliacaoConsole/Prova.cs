63a7e086eaa12f6ec73293f6947c5bc8dc8a0efe
Italo Nunes Leite <italonleite@gmail.com>
2020-03-08 13:05:57 -0300

    - criando classes iniciais

---
+++ AvaliacaoConsole/Prova.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvaliacaoConsole
+{
+    public class Prova
+    {
+        public int IdProva { get; set; }
+        public string Titulo { get; set; }
+
+        public Prova(int idProva, string titulo)
+        {
+            IdProva = idProva;
+            Titulo = titulo;
+        }
+
+    }
+}
---
+++ AvaliacaoConsole/Questao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvaliacaoConsole
+{
+    public class Questao
+    {
+        public int IdQuestao { get; set; }
+        public string Enunciado { get; set; }
+        public decimal Pontos { get; set; }
+        public Prova Prova { get; set; }
+        public Tipo Tipo { get; set; }
+
+        public Questao(int idQuestao, string enunciado, decimal pontos, Prova prova, Tipo tipo)
+        {
+            IdQuestao = idQuestao;
+            Enunciado = enunciado;
+            Pontos = pontos;
+            Prova = prova;
+            Tipo = tipo;
+        }
+    }
+}
