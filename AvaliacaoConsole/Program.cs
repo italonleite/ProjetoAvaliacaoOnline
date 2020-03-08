5c04e7421f2f4ed22b51a842f2efa43b42fd72d3
Italo Nunes Leite <italonleite@gmail.com>
2020-03-08 11:39:34 -0300

    - adicionando projeto console

---
+++ AvaliacaoConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AvaliacaoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
