a2bade35cfc50ba22d83d926743f61383e454aaf
Italo Nunes Leite <italonleite@gmail.com>
2020-03-14 14:56:29 -0300

    Criando classe CandidadtoDAL

---
+++ Avaliacao.Infra/CandidatoDAL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using Avaliacao.Domain;
+
+namespace Avaliacao.Infra
+{
+    public class CandidatoDAL
+    {
+		public CandidatoDAL()
+		{
+
+		}
+		public void Cadastrar(Candidato c) {
+			try
+			{
+				using (SqlConnection conn = new SqlConnection("Data Source=.\\SQLNOVO;Initial Catalog=AvaliaçãoReserveSystem;Integrated Security=SSPI"))
+				{
+					conn.Open();
+					string query = "insert into candidato(NOME, EMAIL, ID_PROVA)" + "values(@NOME, @EMAIL, @ID_PROVA)";
+					SqlCommand cmd = new SqlCommand(query, conn);
+					cmd.Parameters.AddWithValue("@Nome", c.Nome);
+					cmd.Parameters.AddWithValue("@Email", c.Email);
+					cmd.Parameters.AddWithValue("@ID_PROVA", c.Prova.IdProva);
+
+					cmd.CommandType = CommandType.Text;
+					cmd.ExecuteNonQuery();
+				}
+			}
+			catch (Exception)
+			{
+
+				throw;
+			}
+        }
+    }
+}
