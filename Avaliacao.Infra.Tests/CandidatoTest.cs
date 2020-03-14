b9763919e0e5e83834353658e705e2a04e5fba65
Italo Nunes Leite <italonleite@gmail.com>
2020-03-14 14:59:40 -0300

    Criando candidato Tests

---
+++ Avaliacao.Infra.Tests/CandidatoTest.cs
@@ -0,0 +1,29 @@
+﻿using Avaliacao.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Avaliacao.Infra.Tests
+{
+    public class CandidatoTest
+    {
+        [Fact(DisplayName = "Cadastrar Candidato")]
+        public void Cadastrar_DeveAdicionar()
+        {
+            var p = new Prova(1, "Prova");
+            var c = new Candidato(1, "ItaloCandidato", "italite@gmail.com", p);
+            
+            var cd = new CandidatoDAL();
+
+            cd.Cadastrar(c);
+
+
+
+        }
+
+
+     
+
+    }
+}
