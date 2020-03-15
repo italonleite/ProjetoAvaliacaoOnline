cb2af4fe20d555fdc96739b7e921012f6374115c
Italo Nunes Leite <italonleite@gmail.com>
2020-03-14 23:31:14 -0300

    Criando questãoDAl e testes

---
+++ Avaliacao.Infra.Tests/QuestaoTests.cs
@@ -0,0 +1,31 @@
+﻿using Avaliacao.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Avaliacao.Infra.Tests
+{
+    public class QuestaoTests
+    {
+        [Fact(DisplayName = "Cadastrar Questão")]
+        [Trait("Questão", "Cadastrar Questão")]
+        public void Trocar_Nome_Metodo()
+        {
+            // Arrange
+            var p = new Prova(10,"titulo");
+            var t = new Tipo(1, "TipoXYZ");
+            var q = new Questao(1, "EnunciadoX", 10, p, t);
+
+            var qd = new QuestaoDAL();
+
+            qd.Cadastrar(q, p, t);
+
+
+            // Act
+
+            // Assert
+
+        }
+    }
+}
---
+++ Avaliacao.Infra/QuestaoDAL.cs
@@ -0,0 +1,43 @@
+﻿using Avaliacao.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Avaliacao.Infra
+{
+    public class QuestaoDAL
+    {
+        public QuestaoDAL()
+        {
+
+        }
+
+        public void Cadastrar(Questao q, Prova p, Tipo t)
+        {
+            try
+            {
+
+                using (SqlConnection conn = new SqlConnection("Data Source=.\\SQLNOVO;Initial Catalog=AvaliaçãoReserveSystem;Integrated Security=SSPI"))
+                {
+                    conn.Open();
+                    string query = "insert into Questao(ENUNCIADO, PONTOS, ID_PROVA, ID_TIPO)" + "values(@ENUNCIADO, @PONTOS, @ID_PROVA, @ID_TIPO)";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@ENUNCIADO", q.Enunciado);
+                    cmd.Parameters.AddWithValue("@PONTOS", q.Pontos);
+                    cmd.Parameters.AddWithValue("@ID_PROVA", p.IdProva);
+                    cmd.Parameters.AddWithValue("@ID_TIPO", t.IdTipo);
+
+                    cmd.CommandType = CommandType.Text;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+    }
+}
